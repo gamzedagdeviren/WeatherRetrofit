9e2f370750a9b4e880890c03d69678536a3d9ae2
Gamze Dagdeviren <gamzed2022@gmail.com>
2022-02-17 21:58:35 -0500

    Delete app/src/main/java/com/kotlin/weatherretrofit/db directory

--- app/src/main/java/com/kotlin/weatherretrofit/db/WeatherDao.kt
+++
@@ -1,9 +0,0 @@
-package com.kotlin.weatherretrofit.db
-
-import androidx.room.*
-
-@Dao
-interface WeatherDao {
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun updateInsert()
-}
